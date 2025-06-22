a97e69bbdac4dc5f1b6bcef19c63a6a976d009a0
Dustin Hendrickson <dustin.hendrickson@gmail.com>
2025-06-22 10:32:29 -0700

    Adding more Ollama API support settings to the Conf

--- src/mod-ollama-chat_config.h
+++ src/mod-ollama-chat_config.h
@@ -6,51 +6,59 @@
 #include <vector>
 #include "ScriptMgr.h"  // Ensure WorldScript is defined
 
-extern float      g_SayDistance;
-extern float      g_YellDistance;
-extern float      g_GeneralDistance;
-extern uint32_t   g_PlayerReplyChance;
-extern uint32_t   g_BotReplyChance;
-extern uint32_t   g_MaxBotsToPick;
-extern std::string g_OllamaUrl;
-extern std::string g_OllamaModel;
+extern float            g_SayDistance;
+extern float            g_YellDistance;
+extern float            g_GeneralDistance;
+extern uint32_t         g_PlayerReplyChance;
+extern uint32_t         g_BotReplyChance;
+extern uint32_t         g_MaxBotsToPick;
+extern std::string      g_OllamaUrl;
+extern std::string      g_OllamaModel;
+extern uint32_t         g_OllamaNumPredict;
+extern float            g_OllamaTemperature;
+extern float            g_OllamaTopP;
+extern float            g_OllamaRepeatPenalty;
+extern uint32_t         g_OllamaNumCtx;
+extern std::string      g_OllamaStop;
+extern std::string      g_OllamaSystemPrompt;
+extern std::string      g_OllamaSeed;
 
-extern uint32_t   g_MaxConcurrentQueries;
+extern uint32_t         g_MaxConcurrentQueries;
 
-extern bool       g_Enable;
-extern bool       g_EnableRandomChatter;
-extern bool       g_DebugEnabled;
-extern uint32_t   g_MinRandomInterval;
-extern uint32_t   g_MaxRandomInterval;
-extern float      g_RandomChatterRealPlayerDistance;
-extern uint32_t   g_RandomChatterBotCommentChance;
-extern uint32_t   g_RandomChatterMaxBotsPerPlayer;
+extern bool             g_Enable;
+extern bool             g_EnableRandomChatter;
+extern bool             g_DebugEnabled;
+extern uint32_t         g_MinRandomInterval;
+extern uint32_t         g_MaxRandomInterval;
+extern float            g_RandomChatterRealPlayerDistance;
+extern uint32_t         g_RandomChatterBotCommentChance;
+extern uint32_t         g_RandomChatterMaxBotsPerPlayer;
 
-extern bool       g_EnableRPPersonalities;
+extern bool             g_EnableRPPersonalities;
 
 extern std::unordered_map<uint64_t, std::unordered_map<uint64_t, std::deque<std::pair<std::string, std::string>>>> g_BotConversationHistory;
-extern std::mutex g_ConversationHistoryMutex;
-extern uint32_t   g_MaxConversationHistory;
-extern uint32_t   g_ConversationHistorySaveInterval;
-extern time_t     g_LastHistorySaveTime;
+extern std::mutex       g_ConversationHistoryMutex;
+extern uint32_t         g_MaxConversationHistory;
+extern uint32_t         g_ConversationHistorySaveInterval;
+extern time_t           g_LastHistorySaveTime;
 
-extern std::string g_ChatHistoryHeaderTemplate;
-extern std::string g_ChatHistoryLineTemplate;
-extern std::string g_ChatHistoryFooterTemplate;
-extern bool g_EnableChatHistory;
+extern std::string      g_ChatHistoryHeaderTemplate;
+extern std::string      g_ChatHistoryLineTemplate;
+extern std::string      g_ChatHistoryFooterTemplate;
+extern bool             g_EnableChatHistory;
 
-extern std::string g_RandomChatterPromptTemplate;
+extern std::string      g_RandomChatterPromptTemplate;
 
 extern std::unordered_map<uint64_t, std::string> g_BotPersonalityList;
 extern std::unordered_map<std::string, std::string> g_PersonalityPrompts;
 extern std::vector<std::string> g_PersonalityKeys;
 
-extern std::string g_ChatPromptTemplate;
-extern std::string g_ChatExtraInfoTemplate;
+extern std::string      g_ChatPromptTemplate;
+extern std::string      g_ChatExtraInfoTemplate;
 
 extern std::vector<std::string> g_BlacklistCommands;
 
-extern std::string g_DefaultPersonalityPrompt;
+extern std::string      g_DefaultPersonalityPrompt;
 
 extern std::vector<std::string> g_EnvCommentCreature;
 extern std::vector<std::string> g_EnvCommentGameObject;
